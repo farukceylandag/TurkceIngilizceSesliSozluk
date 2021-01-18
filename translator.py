ade81ef5bb84db4f9316ed34dcbb78021b877ebc
farukceylandag <39407440+farukceylandag@users.noreply.github.com>
2021-01-18 21:22:12 +0300

    Update translator.py

--- translator.py
+++ translator.py
@@ -4,11 +4,9 @@ import googletrans
 import pytesseract
 
 img = cv.imread('test.png', 0)
-cv.imshow("Resim",img)
-
 img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-ret, img_binary = cv.threshold(img,100, 255, cv.THRESH_BINARY)
 
+ret, img_binary = cv.threshold(img,100, 255, cv.THRESH_BINARY)
 
 text = pytesseract.image_to_string(img_binary)
 
@@ -17,23 +15,18 @@ k = p.translate(text,src='tr', dest='en')
 translated = str(k.text)
 
 with open('translate.txt',mode ='w') as file:    
-    if(file != ""):
-        file.write("Türkçe:\n"+text)
-        print("\nTürkçe:\n")
-        print(text)
-        file.write("\nEnglish:\n"+translated)
-        print("\nEnglish:\n")
-        print(translated)
+    file.write("Türkçe:\n"+text)
+    print("\nTürkçe:\n")
+    print(text)
+    file.write("\nEnglish:\n"+translated)
+    print("\nEnglish:\n")
+    print(translated)
 
 
 engine = pyttsx3.init() 
 engine.say(translated) 
 engine.runAndWait()
 
-
-cv.waitKey(10)
+cv.imshow("Resim",img)
+cv.waitKey(0)
 cv.destroyAllWindows()
-
-
-
-
